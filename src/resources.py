71b2bce328e007b073117cc9241fec5e3c0ddfa1
courtneihayes <courtneichayes@gmail.com>
2026-01-01 12:49:55 -0600

    Create resources.py

---
+++ src/resources.py
@@ -0,0 +1,7 @@
+RESOURCES = {
+    1: [{"name": "CamScanner", "link": "https://www.camscanner.com/"}],
+    2: [{"name": "YWCA Baton Rouge", "link": "https://www.ywca.org/"}],
+    3: [{"name": "One Breath Project", "link": "https://www.onebreathproject.org/"}],
+    4: [{"name": "Mending the Soul RMBS", "link": "https://www.mendingthesoulrmbs.com/healing-services/"}],
+    5: [{"name": "University of Phoenix", "link": "https://www.phoenix.edu/"}]
+}
